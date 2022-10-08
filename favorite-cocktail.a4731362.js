a6bd44c40f83f187310e7df2a4f2888881150802
Amfiteatr42 <108868537+Amfiteatr42@users.noreply.github.com>
2022-10-08 11:21:16 +0000

    Deploying to gh-pages from @ Amfiteatr42/Cocktails-TeamProject-JS@bb1853f6ca5490cd2a9c759e2e7172a04a0f6d9c 🚀

--- favorite-cocktail.a4731362.js
+++
@@ -1,2 +0,0 @@
-!function(){var o;console.log("helloworld"),console.log((o="gin-Small.png",fetch("https://www.thecocktaildb.com/images/ingredients/".concat(o)).then((function(o){if(!o.ok)throw new Error(o.status);return o.url}))).then(console.log))}();
-//# sourceMappingURL=favorite-cocktail.a4731362.js.map
--- favorite-cocktail.b712e05c.js
+++
@@ -1,2 +0,0 @@
-var o;console.log("helloworld"),console.log((o="gin-Small.png",fetch(`https://www.thecocktaildb.com/images/ingredients/${o}`).then((o=>{if(!o.ok)throw new Error(o.status);return o.url}))).then(console.log));
-//# sourceMappingURL=favorite-cocktail.b712e05c.js.map
